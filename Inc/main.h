37490e3357c81e2688103c0d4f47adaa27cd0471
DESKTOP-VJ67QVJ\Administrator <cofowan@163.com>
2019-11-06 17:50:02 +0800

    调试信息，有关pid参数，以及电压目标，以此为基础制定二维数组，进行中断判断控制

--- Inc/main.h
+++ Inc/main.h
@@ -261,12 +261,58 @@ __STATIC_INLINE  float pwm_get_freq(void)
 //#define MYP			( 0.30f )
 //#define MYI			( 0.20f )
 //#define MYD			( 0.00f )
-#define MYP			( 0.35f )
-#define MYI			( 0.40f )
+/*** 63V @3.5A ***/
+//#define MYP			( 0.235f )
+//#define MYI			( 0.240f )
+//#define MYD			( 0.00f )
+//#define TARGETVOL   ( 63.0f )
+/*****/
+/*** 70V @4A ***/
+//#define MYP			( 0.135f )
+//#define MYI			( 0.140f )
+//#define MYD			( 0.00f )
+//#define TARGETVOL   ( 70.0f )
+/*****/
+/*** 80V @4.5A ***/
+#define MYP			( 0.045f )
+#define MYI			( 0.025f )
 #define MYD			( 0.00f )
-#define TARGETVOL   ( 62.0f )
+#define TARGETVOL   ( 80.0f )
+/*****/
+/*	4.5A以上切换时肯定会产生中断信号，当接收到中断信号时，
+	1：	首先将频率设定为84K,然后保持2秒钟，
+		之后检测其接收到的电流，电压，中断信号：
+		若没有发生中断信号了，表明足够供应：
+											然后查电流：
+											电流<2.5A,电流<3.5A,电流<5A依次对准二维数组表格，
+											里面分别对应着对应的pid与设定电压参数
+		若依然产生中断信号，则执行2
+		
+	2：将频率调节为83K,然后保持2秒钟，之后查电流，电压，中断信号：
+		若没有产生中断信号，表明终于够供应：
+											然后查电流：
+											电流<2.5A,电流<3.5A,电流<5A依次对准二维数组表格，
+											里面分别对应着对应的pid与设定电压参数
+		若依然产生中断信号，一般性况下不应还发生此事，可能出问题了，比如过载了！
+		此时LED灯间烁0.5秒钟，停止工作10秒钟，十秒之后再复位开机。
+*/
 /* USER CODE END Private defines */
-
+/*** 50V @2.5A 1.84V ***/
+//#define MYP			( 0.175 )
+//#define MYI			( 0.125f )
+//#define MYD			( 0.00f )
+//#define TARGETVOL   ( 50.0f )
+/*****/
+//4.0A = 1.95V
+//3.5A = 1.913V
+//3.0A = 1.875V
+//2.5A = 1.84V
+//2.0A = 1.80V
+//1.5A = 1.76V
+//1.0A = 1.73V
+//0.5A = 1.69V
+//0.0A = 1.65V
+/* USER CODE END Private defines */
 #ifdef __cplusplus
 }
 #endif
